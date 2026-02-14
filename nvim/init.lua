1fad92512005ea14de61359582f06e7189e59252
Ilhan Negis <ilhan.negis@gmail.com>
2026-02-14 21:20:01 +0300

    new init

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+vim.lsp.enable("pyright")
+require("config.lazy")
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 17
+config.color_scheme = "Tokyo Night"
+
+config.window_decorations = "RESIZE"
+
+config.hide_tab_bar_if_only_one_tab = true
+-- config.enable_tab_bar = false
+
+-- Finally, return the configuration to wezterm:
+
+return config
